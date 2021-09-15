90e2627bbae2e3eb09f632ad0edc9fd6cd024fda
christian.mcardle <christian.mcardle@vonage.com>
2021-09-15 17:47:51 +0100

    Add Auth0 auth. Fix contentful to use environment variables.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,15 +1,23 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "TwyfordScouts",
+    siteUrl: "https://www.twyfordscouts.org.uk",
+    title: "1st Twyford Scout Group",
   },
   plugins: [
     {
       resolve: "gatsby-source-contentful",
       options: {
-        accessToken: "bPdv3A4oG_H9wfLKn_g-ktCjuw6ThkmgNBlLDZzz7vQ",
-        spaceId: "amtvha5r3fwq",
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
         enableTags: true
+      },
+    },
+    {
+      resolve: "gatsby-theme-auth0",
+      options: {
+        domain: process.env.AUTH0_DOMAIN,
+        clientID: process.env.AUTH0_CLIENT_ID,
+        redirectUri: process.env.AUTH0_CALLBACK_URL,
       },
     },
     "gatsby-plugin-gatsby-cloud",
