49517f1b5bfe4330948c8d84ceaa1e6c47229f62
christian.mcardle <christian.mcardle@vonage.com>
2021-09-16 14:24:51 +0100

    Prettify leader pages

--- src/pages/leaders.js
+++ src/pages/leaders.js
@@ -2,6 +2,16 @@ import React from "react"
 import { Router } from "@reach/router"
 import { login, logout, isAuthenticated, getProfile } from "../utils/auth"
 import { Link } from "gatsby"
+import { StaticImage } from 'gatsby-plugin-image'
+import {
+  container,
+  topBar,
+  navLinkItem,
+  navLinkText,
+  navLinkItemCurrent,
+  profilePicture,
+  content
+} from './leaders.module.css'
 
 const Home = ({ user }) => {
     return <p>Hi, {user.name ? user.name : "friend"}!</p>
@@ -16,24 +26,33 @@ const Leaders = () => {
     }
 
   const user = getProfile()
-    return (
-    <>
-        <nav>
-          <Link to="/leaders">Leaders Home</Link>{" "}
-          <Link to="/leaders/notices">Notices</Link>{" "}
-          <Link to="/leaders/news">News Stories</Link>{" "}
-          <a href="#logout"
+  console.log(user)
+  return (
+    <div className={container}>
+        <nav className={topBar}>
+          <Link to="/" className={navLinkItem} activeClassName={navLinkItemCurrent}>
+            <StaticImage className={navLinkItem}
+              alt="1st Twyford Scouts"
+              src="../images/Scouts1stTwyfordLinearWhite96px.png"/>
+          </Link>
+          <Link className={navLinkItem} to="/leaders"><div className={navLinkText}>Leaders Home</div></Link>{" "}
+          <Link className={navLinkItem} to="/leaders/notices"><div className={navLinkText}>Notices</div></Link>{" "}
+          <Link className={navLinkItem} to="/leaders/news"><div className={navLinkText}>News Stories</div></Link>{" "}
+          <a className={navLinkItem} href="#logout"
             onClick={e => {
               logout()
               e.preventDefault()
-            }}>Log Out</a>
+            }}><div className={navLinkText}>Log Out</div></a>
+            <div className={navLinkItem}>
+              <img className={profilePicture} src={user.picture}/>
+            </div>
         </nav>
         <Router>
         <Home path="/leaders" user={user} />
         <Notices path="/leaders/notices" />
         <News path="/leaders/news" />
         </Router>
-    </>
+    </div>
     )
 }
 
